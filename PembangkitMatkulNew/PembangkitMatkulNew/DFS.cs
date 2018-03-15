a8f3c1173a8e624ea687bbf360d5246f28a2a3f1
Mochamad Alghifari <13516038@std.stei.itb.ac.id>
2018-03-15 16:26:47 +0700

    DFS NYIAHAHAHAHA

---
+++ PembangkitMatkulNew/PembangkitMatkulNew/DFS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PembangkitMatkulNew
+{
+    class DFS
+    {
+        public void DFSM(SimpulDFS S)
+        {
+            Console.WriteLine("Masuk Ke Simpul {0}", S.NamaMatkul);
+            S.Mulai = ++S.Waktu;
+            Console.WriteLine("Set mulai dari {0} menjadi {1}", S.NamaMatkul, S.Mulai);
+            for (int i = 0; i < S.Tetangga.Count; i++)
+            {
+                Console.WriteLine("Cek tetangga {0} dikunjungi : {1}", S.Tetangga[i], S.KumpulanSimpul[S.Tetangga[i]].Dikunjungi);
+                if (!S.KumpulanSimpul[S.Tetangga[i]].Dikunjungi)
+                {
+
+                    DFSM(S.KumpulanSimpul[S.Tetangga[i]]);
+                }
+            }
+            S.Selesai = ++S.Waktu;
+            Console.WriteLine("Set selesai dari {0} menjadi {1}", S.NamaMatkul, S.Selesai);
+            S.Dikunjungi = true;
+        }
+    }
+}
--- PembangkitMatkulNew/PembangkitMatkulNew/Program.cs
+++ PembangkitMatkulNew/PembangkitMatkulNew/Program.cs
@@ -10,6 +10,46 @@ namespace PembangkitMatkulNew
     {
         static void Main(string[] args)
         {
+            SimpulDFS C1 = new SimpulDFS();
+            C1.NamaMatkul = "C1";
+            C1.Tetangga.Add("C2");
+            C1.Tetangga.Add("C4");
+            SimpulDFS C2 = new SimpulDFS();
+            C2.NamaMatkul = "C2";
+            C2.Tetangga.Add("C5");
+            SimpulDFS C3 = new SimpulDFS();
+            C3.NamaMatkul = "C3";
+            C3.Tetangga.Add("C1");
+            C3.Tetangga.Add("C4");
+            SimpulDFS C4 = new SimpulDFS();
+            C4.NamaMatkul = "C4";
+            C4.Tetangga.Add("C2");
+            C4.Tetangga.Add("C5");
+            SimpulDFS C5 = new SimpulDFS();
+            C5.NamaMatkul = "C5";
+            C1.KumpulanSimpul.Add("C1",C1);
+            C1.KumpulanSimpul.Add("C2", C2);
+            C1.KumpulanSimpul.Add("C3", C3);
+            C2.KumpulanSimpul.Add("C4", C4);
+            C3.KumpulanSimpul.Add("C5", C5);
+            DFS dfs = new DFS();
+            foreach (KeyValuePair<string, SimpulDFS> pair in C1.KumpulanSimpul)
+            {
+                for (int i=0; i<C1.KumpulanSimpul[pair.Key].Tetangga.Count; i++)
+                {
+                    C1.KumpulanSimpul[C1.KumpulanSimpul[pair.Key].Tetangga[i]].SisiMasuk++;
+                }       
+            }
+            foreach (KeyValuePair<string, SimpulDFS> pair in C1.KumpulanSimpul)
+            {
+                if (C1.KumpulanSimpul[pair.Key].SisiMasuk == 0) {
+                    dfs.DFSM(C1.KumpulanSimpul[pair.Key]);
+                }
+            }
+            foreach (KeyValuePair<string, SimpulDFS> pair in C1.KumpulanSimpul)
+            {
+                Console.WriteLine("{0}, {1}, {2}", pair.Key, C1.KumpulanSimpul[pair.Key].Mulai, C1.KumpulanSimpul[pair.Key].Selesai);
+            }
         }
     }
 }
