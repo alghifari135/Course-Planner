16d54513c977e0c7cdc925944a48149ed2083c50
Mochamad Alghifari <13516038@std.stei.itb.ac.id>
2018-03-15 16:48:49 +0700

    delete instances of SimpulDFS

--- PembangkitMatkulNew/PembangkitMatkulNew/Program.cs
+++ PembangkitMatkulNew/PembangkitMatkulNew/Program.cs
@@ -10,28 +10,6 @@ namespace PembangkitMatkulNew
     {
         static void Main(string[] args)
         {
-            SimpulDFS C1 = new SimpulDFS();
-            C1.NamaMatkul = "C1";
-            C1.Tetangga.Add("C2");
-            C1.Tetangga.Add("C4");
-            SimpulDFS C2 = new SimpulDFS();
-            C2.NamaMatkul = "C2";
-            C2.Tetangga.Add("C5");
-            SimpulDFS C3 = new SimpulDFS();
-            C3.NamaMatkul = "C3";
-            C3.Tetangga.Add("C1");
-            C3.Tetangga.Add("C4");
-            SimpulDFS C4 = new SimpulDFS();
-            C4.NamaMatkul = "C4";
-            C4.Tetangga.Add("C2");
-            C4.Tetangga.Add("C5");
-            SimpulDFS C5 = new SimpulDFS();
-            C5.NamaMatkul = "C5";
-            C1.KumpulanSimpul.Add("C1",C1);
-            C1.KumpulanSimpul.Add("C2", C2);
-            C1.KumpulanSimpul.Add("C3", C3);
-            C2.KumpulanSimpul.Add("C4", C4);
-            C3.KumpulanSimpul.Add("C5", C5);
             DFS dfs = new DFS();
             dfs.HitungSisiMasuk(C1);
             dfs.DFSMain(C1);
