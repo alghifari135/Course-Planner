af1e4a3a54b91b22c3b12abf1132510993eeff6e
Mochamad Alghifari <13516038@std.stei.itb.ac.id>
2018-03-18 16:21:17 +0700

    Fixing DFS + Add CetakHasilDFS and CetakHasilBFS method

--- PembangkitMatkulNew/PembangkitMatkulNew/Program.cs
+++ PembangkitMatkulNew/PembangkitMatkulNew/Program.cs
@@ -13,20 +13,10 @@ namespace PembangkitMatkulNew
             BacaFile BF = new BacaFile("filesimpul.txt");
             DFS dfs = new DFS();
             BFS bfs = new BFS();
-            //dfs.HitungSisiMasuk();
             dfs.DFSMain();
             bfs.MulaiBFS();
-            foreach (KeyValuePair<string, SimpulDFS> pair in SimpulDFS.KumpulanSimpul)
-            {
-                Console.WriteLine("{0}, {1}, {2}", pair.Key, SimpulDFS.KumpulanSimpul[pair.Key].Mulai, SimpulDFS.KumpulanSimpul[pair.Key].Selesai);
-            }
-            Console.WriteLine("Urutan BFS :");
-            int i = 1;
-            foreach (SimpulBFS simpul in BFS.DaftarTerurutBFS)
-            {
-                Console.WriteLine("{0}. {1}", i, simpul.Nama);
-                i++;
-            }
+            dfs.CetakHasilDFS();
+            bfs.CetakHasilBFS();
         }
     }
 }
