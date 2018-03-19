dc93e467ec8a9dd482f47674741c07b3651f33dd
raflyalk <raflyalk@gmail.com>
2018-03-19 15:21:29 +0700

    Done Visualizing

--- PembangkitMatkulNew/PembangkitMatkulNew/Program.cs
+++ PembangkitMatkulNew/PembangkitMatkulNew/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 using System.Threading.Tasks;
 
 namespace PembangkitMatkulNew
@@ -17,6 +18,31 @@ namespace PembangkitMatkulNew
             bfs.MulaiBFS();
             dfs.CetakHasilDFS();
             bfs.CetakHasilBFS();
+
+            //create a form 
+            Form form = new Form();
+            //create a viewer object 
+            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            //create a graph object 
+            Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
+            //create the graph content 
+            foreach(SimpulBFS simpul in BFS.DaftarTerurutBFS)
+            {
+                foreach(string tetangga in simpul.Tetangga)
+                {
+                    graph.AddEdge(simpul.Nama, tetangga);
+                }
+            }
+            //bind the graph to the viewer 
+            viewer.Graph = graph;
+            //associate the viewer with the form 
+            form.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            form.Controls.Add(viewer);
+            form.ResumeLayout();
+            //show the form 
+            form.ShowDialog();
         }
+    
     }
 }
